34828b63d9dcfa3ee211f3100e657c7d01f5b610
ASproson <a.sproson@outlook.com>
2024-05-30 19:33:35 +0100

    Removes old ref

--- src/components/FoxUmbrellas.tsx
+++ src/components/FoxUmbrellas.tsx
@@ -5,19 +5,10 @@ import { Nav } from "./Nav";
 import { Personalized } from "./Personalized";
 import { Discover } from "./Discover";
 
-const ref =
-  "https://dribbble.com/shots/24027033-Fox-Umbrellas-Luxury-Sophisticated-Website-Design-Concept";
-
 export const FoxUmbrellas = () => {
   return (
     <div className="min-h-screen" style={{ backgroundColor: "#e6e6e6" }}>
       <div className="py-10 px-4 sm:px-14 flex justify-center">
-        {/* <div className="absolute top-3 text-sm text-gray-500 space-x-6">
-          <Link to="/">Home</Link>
-          <a href={ref} target="_blank">
-            Reference
-          </a>
-        </div> */}
         <div className="bg-white h-full rounded-sm shadow-xl overflow-hidden w-[1000px] relative">
           <div className="px-12 pb-10">
             <div className="relative flex justify-end items-center pt-10">
